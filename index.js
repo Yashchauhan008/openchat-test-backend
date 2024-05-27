ada452d05a465808c83b9a9d60783ff447705499
Yash Chauhan <yashchauhan1775@gmail.com>
2024-05-27 19:52:13 +0530

    base url

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 require('dotenv').config();
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const dburi = process.env.MONGO_URI
-const PORT = process.env.PORT
-const postRouter = require('./src/routers/post.route')
-const commentRouter = require('./src/routers/comment.route')
-const replyRouter = require('./src/routers/reply.route')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use("/post",postRouter)
-app.use("/comment",commentRouter)
-app.use("/reply",replyRouter)
-app.use(express.json())
+const app = express();
+const dburi = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+const API_BASE_URL = process.env.API_BASE_URL;
 
-app.get((req,res)=>{
-    res.send("Hello World")
-})
+const postRouter = require('./src/routers/post.route');
+const commentRouter = require('./src/routers/comment.route');
+const replyRouter = require('./src/routers/reply.route');
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/post", postRouter);
+app.use("/comment", commentRouter);
+app.use("/reply", replyRouter);
+app.use(express.json());
 
+// Example usage of API_BASE_URL
+app.get('/api-url', (req, res) => {
+    res.send(`API Base URL is ${API_BASE_URL}`);
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
 
 mongoose.connect(dburi)
-    .then(()=>{
-        app.listen(PORT,()=>{
-            console.log(`:::: Server is running on ${PORT}`)
-        })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`:::: Server is running on ${PORT}`);
+        });
     })
-    .catch((error)=>{
-        console.log(`Error:${error}`)
-    })
+    .catch((error) => {
+        console.log(`Error: ${error}`);
+    });
